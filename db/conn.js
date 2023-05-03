a9c33b0f09c698aff1099d0e1b4066b763041335
bhushanvp <bhushanvp.7@gmail.com>
2023-05-03 16:27:58 +0530

    changed gitignore

--- db/conn.js
+++ db/conn.js
@@ -3,6 +3,7 @@ const mysql = require("mysql2")
 module.exports = mysql.createConnection({
     host: 'localhost',
     user: 'root',
+    password: 'redhat',
     database: 'db1'
 })
 .on("connect", () => {
